091755fb2b18219937121fcbd3b5d4442e0d1aad
Cubin <78607391+NiceCubin@users.noreply.github.com>
2021-10-16 14:37:28 +0700

    refactored

--- main.py
+++ main.py
@@ -3,15 +3,16 @@
 import random as rd
 
 WORDS = ["Pig", "Cow", "Horse"]
-guesses = 6
+
 word = rd.choice(WORDS)
 hiddenword = ["_"] * len(word)
+guesses = 6
 tried = []
   
 while "_" in hiddenword and guesses > 0:
   print(*hiddenword)
-  guess = input("Your Guess: ")
-  guess = guess.lower().strip()
+
+  guess = input("Your Guess: ").lower().strip()
 
   if len(guess) != 1 and len(guess) != len(word):
     print("That Guess is Invalid!")
@@ -26,25 +27,36 @@ while "_" in hiddenword and guesses > 0:
     continue
 
   if len(guess) == 1 and guess in word.lower():
-    for x in range(len(word)):
-      if word.lower()[x] == guess: 
-        hiddenword[x] = word[x]
     print("Yes! That's Correct\n")
-    if not "_" in hiddenword: print(*hiddenword)
+
+    for i in range(len(word)):
+      if word.lower()[i] == guess: 
+        hiddenword[i] = word[i]
+
+    if not "_" in hiddenword:
+      print(*hiddenword)
 
   elif guess == word.lower():
-    for x in range(len(word)): 
-      hiddenword[x] = word[x]
     print("Yes That's The Word!\n")
-    if not "_" in hiddenword: print(*hiddenword)
+
+    for i in range(len(word)):
+      hiddenword[i] = word[i]
+
+    if not "_" in hiddenword:
+      print(*hiddenword)
 
   else:
-    guesses -= 1
     print("No! That's Incorrect\n")
-    if guesses == 1: print("You Have 1 Wrong Guess Left!")
-    else: print(f"You Have {guesses} Wrong Guesses Left.")
+
+    guesses -= 1
+    if guesses == 1:
+      print("You Have 1 Wrong Guess Left!")
+    else:
+      print(f"You Have {guesses} Wrong Guesses Left.")
 
   tried.append(guess)
 
-if guesses > 0: print("You Won!")
-else: print("You Lost..")
+if guesses > 0:
+ print("You Won!")
+else:
+ print("You Lost..")
